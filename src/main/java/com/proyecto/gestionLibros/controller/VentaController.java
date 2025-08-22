573430d2f1adcdcc76ed39cec43b4feb162def37
AliSonNicol <alisonllerenallanos@gmail.com>
2025-08-22 15:57:25 -0500

    Se actualizan los controladores de clientes venta y detalle.

--- src/main/java/com/proyecto/gestionLibros/controller/VentaController.java
+++ src/main/java/com/proyecto/gestionLibros/controller/VentaController.java
@@ -1,5 +1,46 @@
 package com.proyecto.gestionLibros.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.proyecto.gestionLibros.Service.VentaService;
+import com.proyecto.gestionLibros.entity.Venta;
+
+@RestController
+@RequestMapping("/api/ventas")
 public class VentaController {
 
+	@Autowired
+	private VentaService ventaService;
+	
+	@GetMapping
+	public List<Venta> listarVentas(){
+		return ventaService.listarTodos();
+	}
+	
+	@PostMapping
+	public Venta crearVenta(@RequestBody Venta venta) {
+		return ventaService.guardar(venta);
+	}
+	
+	@GetMapping("/{id}")
+	public Venta obtenerVenta(@PathVariable Long id) {
+		return ventaService.obtenerPorId(id);
+	}
+	
+	@DeleteMapping("/{id}")
+	public void eliminarVenta(@PathVariable Long id) {
+		ventaService.eliminar(id);
+	}
+	
+	
+	
 }
---
+++ src/main/java/com/proyecto/gestionLibros/controller/VentaDetalleController.java
@@ -0,0 +1,46 @@
+package com.proyecto.gestionLibros.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.proyecto.gestionLibros.Service.VentaDetalleService;
+import com.proyecto.gestionLibros.entity.VentaDetalle;
+
+@RestController
+@RequestMapping("/api/ventaDetalle")
+public class VentaDetalleController {
+
+	@Autowired
+	private VentaDetalleService ventaDetalleService;
+	
+	
+	@GetMapping
+	public List<VentaDetalle> listarDetalles(){
+		return ventaDetalleService.listarTodos();
+	}
+	
+	@PostMapping
+	public VentaDetalle crearDetalleVenta(@RequestBody VentaDetalle ventaDetalle) {
+		return ventaDetalleService.guardar(ventaDetalle);
+	}
+	
+	
+	@GetMapping("/{id}")
+	public VentaDetalle obtenerVentaDetalle(@PathVariable Long id) {
+		return ventaDetalleService.obtenerPorId(id);
+	}
+	
+	@DeleteMapping
+	public void eliminarDetalleVenta(@PathVariable Long id) {
+		ventaDetalleService.eliminar(id);
+	}
+	
+}
