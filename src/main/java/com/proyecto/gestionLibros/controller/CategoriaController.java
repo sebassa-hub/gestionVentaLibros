67f3277d70f047f88abb3080ce648f4ab22aafd1
sebassa-hub <sebasnaruto32@gmail.com>
2025-08-24 18:13:39 -0500

    corriguendo errores de mi parte

--- src/main/java/com/proyecto/gestionLibros/controller/CategoriaController.java
+++ src/main/java/com/proyecto/gestionLibros/controller/CategoriaController.java
@@ -1,9 +1,54 @@
 package com.proyecto.gestionLibros.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.proyecto.gestionLibros.Service.CategoriaService;
+import com.proyecto.gestionLibros.entity.Categoria;
 
 @CrossOrigin(origins = "http://localhost:4200")
-
+@RestController
+@RequestMapping("/api/categoria")
 public class CategoriaController {
 
+	@Autowired
+	private CategoriaService catServ;
+	
+	@GetMapping
+	public ResponseEntity<List<Categoria>> listarCategoria(){
+		return ResponseEntity.ok(catServ.listar());
+	}
+	
+	@GetMapping("/{id}")
+	public Categoria obtenerCategoria(@PathVariable Long id) {
+		return catServ.obtenerPorId(id);
+	}
+	
+	@PostMapping
+	public ResponseEntity<?> agregarCategoria(@RequestBody Categoria cat){
+		try {
+			return ResponseEntity.ok(catServ.guardar(cat));
+		} catch(Exception e){
+			return ResponseEntity.badRequest().body("Error al guardar la categoria");
+		}
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<?> actualizarCategoria(@RequestBody Categoria cat){
+		try {
+			return ResponseEntity.ok(catServ.actualizar(cat));
+		} catch(Exception e){
+			return ResponseEntity.badRequest().body("Error al actualizar la categoria");
+		}
+	}
 }
--- src/main/java/com/proyecto/gestionLibros/controller/EditorialController.java
+++ src/main/java/com/proyecto/gestionLibros/controller/EditorialController.java
@@ -1,9 +1,54 @@
 package com.proyecto.gestionLibros.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.proyecto.gestionLibros.Service.EditorialService;
+import com.proyecto.gestionLibros.entity.Editorial;
 
 @CrossOrigin(origins = "http://localhost:4200")
-
+@RestController
+@RequestMapping("/api/editorial")
 public class EditorialController {
 
+	@Autowired
+	private EditorialService editServ;
+	
+	@GetMapping
+	public ResponseEntity<List<Editorial>> listarEditorial(){
+		return ResponseEntity.ok(editServ.listar());
+	}
+	
+	@GetMapping("/{id}")
+	public Editorial obtenerEditorial(@PathVariable Long id) {
+		return editServ.obtenerPorId(id);
+	}
+	
+	@PostMapping
+	public ResponseEntity<?> agregarEditorial(@RequestBody Editorial edit){
+		try {
+			return ResponseEntity.ok(editServ.guardar(edit));
+		} catch(Exception e){
+			return ResponseEntity.badRequest().body("Error al guardar la editorial");
+		}
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<?> actualizarEditorial(@RequestBody Editorial edit){
+		try {
+			return ResponseEntity.ok(editServ.actualizar(edit));
+		} catch(Exception e){
+			return ResponseEntity.badRequest().body("Error al actualizar la editorial");
+		}
+	}
 }
