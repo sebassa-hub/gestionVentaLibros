904aca9d8e0fc694c98112c32e4b1164fe7bc446
Esteban <esteban.fernandeze06@gmail.com>
2025-08-24 01:57:47 -0500

    Commit Usuario

---
+++ src/main/java/com/proyecto/gestionLibros/entity/Usuario.java
@@ -0,0 +1,58 @@
+package com.libro.demo.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "usuario")
+public class Usuario {
+	@Id
+	@Column(name = "idusuario")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name = "nombres")
+	private String nombres;
+	
+	@Column(name = "apellidos")
+	private String apellidos;
+	
+	@Column(name = "usuario")
+	private String username;
+	
+	@Column(name = "clave")
+	private String clave;
+	
+	@ManyToOne
+	@JoinColumn(name= "idrol")
+	private Rol rol;
+	
+	public Usuario() {
+			
+	}
+
+	public Usuario(String nombres, String apellidos,String username ,String clave, Rol rol) {
+		this.nombres=nombres;
+		this.apellidos=apellidos;
+		this.username=username;
+		this.clave=clave;
+		this.rol=rol;
+	
+	}
+
+	public Object getPassword() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
